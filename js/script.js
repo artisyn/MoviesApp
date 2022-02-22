1213e122811a64bf7b7184d0edbc822ef0b9a323
artisyn <artioms1@gmail.com>
2022-02-22 23:32:13 +0200

    nav and search

--- js/script.js
+++ js/script.js
@@ -4,6 +4,17 @@ import Spinner from './spinner.js';
 
 import * as Request from './request.js';
 
+const searchCheck = document.querySelector('#search__check');
+const searchLogo = document.querySelector('.search__logo');
+const searchInput = document.querySelector('.input__text');
+
+searchCheck.addEventListener('change', () => {
+  searchLogo.classList.toggle('rotate');
+  searchInput.classList.toggle('no__opacity');
+  searchInput.classList.toggle('no__click');
+  if (searchCheck.checked === true) searchInput.focus();
+});
+
 const testImg = document.querySelector('.image');
 
 Request.listOfGenres('movie');
