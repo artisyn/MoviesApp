f75423c6e328252942308bd62794370689fe2269
artisyn <artioms1@gmail.com>
2022-02-23 04:38:34 +0200

    side nav

--- js/script.js
+++ js/script.js
@@ -7,12 +7,18 @@ import * as Request from './request.js';
 const searchCheck = document.querySelector('#search__check');
 const searchLogo = document.querySelector('.search__logo');
 const searchInput = document.querySelector('.input__text');
+const slidingCheck = document.querySelector('#movie__checkbox');
+const slidingMenu = document.querySelector('.sliding__menu');
 
 searchCheck.addEventListener('change', () => {
   searchLogo.classList.toggle('rotate');
   searchInput.classList.toggle('no__opacity');
   searchInput.classList.toggle('no__click');
   if (searchCheck.checked === true) searchInput.focus();
+});
+
+slidingCheck.addEventListener('change', () => {
+  slidingMenu.classList.toggle('slide__left');
 });
 
 const testImg = document.querySelector('.image');
